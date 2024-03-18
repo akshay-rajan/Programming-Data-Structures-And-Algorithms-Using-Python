a2c1a5768e71969c4467bdafc796ffcb825209ca
Akshay Rajan <akshayrajan345@gmail.com>
2024-03-18 19:25:03 +0530

    if name main

--- code/gcd.py
+++ code/gcd.py
@@ -1,4 +1,6 @@
 def gcd(a, b):
     return gcd(b, a % b) if b != 0 else a
 
-print(gcd(int(input("a: ")), int(input("b: "))))
+
+if __name__ == "__main__":
+    print(gcd(int(input("a: ")), int(input("b: "))))
