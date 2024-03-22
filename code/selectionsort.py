2ecb9af97e68180374abb4ab5373f3bc89c74b05
Akshay Rajan <akshayrajan345@gmail.com>
2024-03-22 21:34:11 +0530

    Add Selection Sort

---
+++ code/selectionsort.py
@@ -0,0 +1,21 @@
+# Select the next element in the sorted order and exchange it with the values at the beginning of the list
+
+def selection_sort(arr):
+    # For each element in the array
+    for start in range(len(arr)):
+        # Set the minimum position to the current index
+        minpos = start
+        # For each element in the array starting from the current index
+        for i in range(start, len(arr)):
+            # Find the minimum value in the array
+            if arr[i] < arr[minpos]:
+                minpos = i
+        # Swap the minimum value with the current index
+        arr[start], arr[minpos] = arr[minpos], arr[start]
+        
+
+if __name__ == "__main__":
+    arr = [64, 25, 12, 22, 11]
+    print("Original: ", arr)
+    selection_sort(arr)
+    print("Sorted: ", arr)
